af12ffd04df6506706377e321ce4877978a4de03
Patrick <patrick.ester@gmail.com>
2016-05-02 10:05:51 -0700

    Snake Game
    Start making snake game

---
+++ snake-game.rkt
@@ -0,0 +1,38 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname snake-game) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#lang racket
+(require 2htdp/universe 2htdp/image)
+
+(struct pit (snake goos))
+(struct snake (dir segs))
+(struct posn (x y))
+(struct goo (loc expire))
+
+(define (start-snake)
+  (big-bang (pit (snake "right" (list (posn 1 1)))
+                 (list (fresh-goo)
+                       (fresh-goo)
+                       (fresh-goo)
+                       (fresh-goo)
+                       (fresh-goo)
+                       (fresh-goo)))
+            (on-tick next-pit TICK-RATE)
+            (on-key direct-snake)
+            (to-draw render-pit)
+            (stop-when dead? render-end)))
+
+(define (next-pit w)
+  (define snake (pit-snake w))
+  (define goos (pit-goos w))
+  (define goo-to-eat (can-eat snake goos))
+  (if goo-to-eat
+      (pit (grow snake) (age-goo (eat goos goo-to-eat)))
+      (pit (slither snake) (age-goo goos))))
+
+(define (can-eat snake goos)
+  (cond [(empty? goos) #f]
+        [else (if (close? (snake-head snake) (first goos))
+                  (first goos)
+                  (can-eat snake (rest goos)))]))
+
